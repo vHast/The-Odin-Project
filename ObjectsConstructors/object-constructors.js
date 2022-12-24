1bebda863cade923d111640a1988ec633890bbb4
vhast <vhast@1451-26ef-dfb6-b67d-6880-4bc1-07d0-2001.sta.estpak.ee>
2022-12-24 19:58:28 +0200

    Exercise 1 upload

---
+++ ObjectsConstructors/object-constructors.js
@@ -0,0 +1,15 @@
+// Write a constructor for making "Book" objects
+
+function Book(title, author, pages, read) { // The constructor will take 4 arguments to create the new object
+  this.title = title 
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function(){ // This is the syntax to add a function to the object
+    return this.title , this.author, this.pages, this.read // However, I'm not sure if this will be returned
+  };
+}
+
+const newBook = new Book("The Hobbit", "J.R.R. Tolkien", 300, true) // We create a new Book class object with this syntax
+
+console.log(newBook.info);
