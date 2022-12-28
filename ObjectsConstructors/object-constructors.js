d503ebc4c7f4dcde78cc2b218756169aed9e743d
vHast <fedvhast@gmail.com>
2022-12-29 01:53:22 +0200

    Inheritance and Prototype

--- ObjectsConstructors/object-constructors.js
+++ ObjectsConstructors/object-constructors.js
@@ -1,10 +1,10 @@
 // Write a constructor for making "Book" objects
 
 function Book(title, author, pages, read) { // The constructor will take 4 arguments to create the new object
-  this.title = title 
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
   this.info = function(){ // This is the syntax to add a function to the object
     return `${this.title} ${this.author} ${this.pages} ${this.read}`; // However, I'm not sure if this will be returned, it still isn't returned
   };
@@ -13,3 +13,23 @@ function Book(title, author, pages, read) { // The constructor will take 4 argum
 const newBook = new Book("The Hobbit", "J.R.R. Tolkien", 300, true) // We create a new Book class object with this syntax
 
 console.log(newBook.info);
+newBook.info()
+
+// Inheritance and Prototype
+
+function PrintStuff(myDocuments) { // This is a constructor
+  this.documents = myDocuments;
+}
+
+// We add the print() method to PrintStuff prototype property so that other instances(objects) can inherit
+
+PrintStuff.prototype.print = function() {
+  console.log(this.documents)
+}
+
+// Create a new object with the PrintStuff() constructor, thus allowing this new object to inherit PrintStuff's properties and methods
+
+const newPrinter = new PrintStuff("Umbrella document");
+newPrinter.print()
+
+// newPrinter now has .print() function and uses PrintStuff as constructor
